1c67a7b1d076584e9ab5ca4f827a1b8e479e8b87
Younes Belkouchi <belkouchiyounes@gmail.com>
2020-03-09 18:58:55 +0100

    Added tensorboard logger

--- main.py
+++ main.py
@@ -1,54 +1,55 @@
 from model import CustomCNN, weights_init
 from data import ImageDataset, train_transforms, test_transforms
+from utils import Logger
 
 import numpy as np
+from tqdm import tqdm
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
-from tqdm import tqdm
+import torch.utils.tensorboard
 from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
 
 GPU = False
-
-
-def mean_loss(losses, trainset, ep):
-    return sum(losses[-len(trainset):]) / (len(losses) if ep == 0 else len(trainset))
+LOG_EVERY = 1
 
 
 def train(model, loss_fn, optimizer, trainset, valset, n_epochs, scheduler=None, gpu=False):
     # Train
     model.train()
-    losses = []
 
     for ep in range(n_epochs):
         model.train()
         pbar = tqdm(total=len(trainset))
-        for x, y in trainset:
+        for i, (x, y) in enumerate(trainset):
             if gpu:
                 x, y = x.cuda(), y.cuda()
-
             preds = model(x)
             loss = loss_fn(preds, y)
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
 
-            losses.append(loss.item())
+            if i % LOG_EVERY == 0:
+                acc = accuracy_score(torch.argmax(preds.detach().cpu(), dim=1), y.detach().cpu())
+                logger.add_loss("train", loss.item(), i + ep * LOG_EVERY)
+                logger.add_accuracy("train", acc, i + ep * LOG_EVERY)
             pbar.update()
-            pbar.set_description(f"Epoch {ep + 1}, Loss {losses[-1]:.3f} Mean Loss {mean_loss(losses, trainset, ep):.3f}")
+            pbar.set_description(f"Epoch {ep + 1}, Loss {logger.losses[-1]:.3f}")
         if scheduler is not None:
             scheduler.step()
+            logger.add_learning_rate(scheduler.get_lr(), ep)
         result = evaluate(model, valset, ep, gpu)
         torch.save({
             "epoch": ep,
             "model": model.state_dict(),
             "optimizer": optimizer.state_dict(),
-            "loss": losses,
+            "loss": logger.losses,
             "result": result
-        }, f"model.{ep}.pth")
+        }, f"models/model.{ep}.pth")
     print("Training Finished")
-    return losses
+    return logger.losses
 
 
 def evaluate(model, valset, n_epoch, gpu=False):
@@ -69,8 +70,11 @@ def evaluate(model, valset, n_epoch, gpu=False):
             true_labels.extend(y)
     true_labels = np.array(true_labels)
     all_preds = np.array(all_preds)
-    res = f"Model validation: Accuracy {accuracy_score(true_labels, all_preds)}, Precision {precision_score(true_labels, all_preds)}, Recall {recall_score(true_labels, all_preds)}, ROC {roc_auc_score(true_labels, all_preds_probas)}"
-    print(res)
+    acc = accuracy_score(true_labels, all_preds)
+    precision = precision_score(true_labels, all_preds)
+    recall = recall_score(true_labels, all_preds)
+    roc = roc_auc_score(true_labels, all_preds_probas)
+    res = f"Model validation: Accuracy {acc}, Precision {precision}, Recall {recall}, ROC {roc}"
     return res
 
 
@@ -86,25 +90,30 @@ def test(mdl):
     return all_preds
 
 
-# Init everything
-train_set = ImageDataset("data/train_manifest.csv", transform=train_transforms)
-train_loader = DataLoader(train_set, batch_size=64, shuffle=True, num_workers=4)
+if __name__ == "__main__":
+    # Init everything
+    train_set = ImageDataset("data/train_manifest.csv", transform=train_transforms)
+    train_loader = DataLoader(train_set, batch_size=64, shuffle=True, num_workers=4)
 
-eval_set = ImageDataset("data/val_manifest.csv", transform=test_transforms)
-eval_loader = DataLoader(eval_set, batch_size=64, shuffle=False, num_workers=4)
+    eval_set = ImageDataset("data/val_manifest.csv", transform=test_transforms)
+    eval_loader = DataLoader(eval_set, batch_size=64, shuffle=False, num_workers=4)
 
-test_set = ImageDataset("data/test_manifest.csv", transform=test_transforms)
-test_loader = DataLoader(test_set, batch_size=64, shuffle=False, num_workers=4)
+    test_set = ImageDataset("data/test_manifest.csv", transform=test_transforms)
+    test_loader = DataLoader(test_set, batch_size=64, shuffle=False, num_workers=4)
 
-model = CustomCNN()
-model.apply(weights_init)
-if GPU:
-    model = model.cuda()
+    model = CustomCNN()
+    model.apply(weights_init)
+    if GPU:
+        model = model.cuda()
 
-criterion = nn.CrossEntropyLoss(reduction="sum")
-criterion = criterion.cuda()
+    criterion = nn.CrossEntropyLoss(reduction="sum")
+    criterion = criterion.cuda()
 
-optimizer = optim.SGD(model.parameters(), lr=0.01)
-exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
+    optimizer = optim.SGD(model.parameters(), lr=0.01)
+    exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
 
-train(model, criterion, optimizer, train_loader, eval_loader, 100, exp_lr_scheduler, GPU)
+    # Log some things
+    logger = Logger("-test1")
+    logger.add_general_data(model, train_loader, )
+
+    train(model, criterion, optimizer, train_loader, eval_loader, 100, exp_lr_scheduler, GPU)
---
+++ utils.py
@@ -0,0 +1,29 @@
+from torch.utils.tensorboard import SummaryWriter
+from torchvision.utils import make_grid
+
+
+class Logger:
+    def __init__(self, exp_name=None):
+        self.writer = SummaryWriter(comment=exp_name)
+        self.train_batch = 0
+        self.train_size = 0
+        self.losses = []
+
+    def add_general_data(self, model, train_loader):
+        images, _ = next(iter(train_loader))
+        self.train_batch = images[0]
+        self.train_size = len(train_loader)
+        grid = make_grid(images[:32])
+        self.writer.add_image("images", grid, 0)
+        self.writer.add_graph(model, (images,))
+        self.writer.flush()
+
+    def add_training_scalars(self, loss, acc, iter_):
+        self.writer.add_scalar(f"train/loss", loss, iter_)
+        self.writer.add_scalar(f"train/acc", acc, iter_)
+        self.losses.append(loss)
+        self.writer.flush()
+
+    def add_learning_rate(self, lr, iter_):
+        self.writer.add_scalar(f"train/learning_rate", lr[0], iter_)
+        self.writer.flush()
